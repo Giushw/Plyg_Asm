503db2727df1fcee1dec68317c2323f8779a6fc6
giuseppe.dellavvocato@vivaticket.com <dellavvocatog@gmail.com>
2019-11-07 15:37:58 +0100

    feat(): external scss

--- plyg_lnd/src/App.vue
+++ plyg_lnd/src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import style from './scss/app.scss';
 
 export default {
   name: 'app',
@@ -15,14 +16,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
