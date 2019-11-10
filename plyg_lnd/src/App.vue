dbcc69a5bfb33c0bb51945d5d118b0aa7913bf1f
Giushw <dellavvocatog@gmail.com>
2019-11-10 21:23:21 +0100

    chore(): webfonts and stuffs

--- plyg_lnd/src/App.vue
+++ plyg_lnd/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainLayout msg="TEst Landing in Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import MainLayout from './components/MainLayout.vue';
 import style from './scss/app.scss';
 
 // use common global styles
@@ -14,7 +14,7 @@ export default {
   style,
   name: 'app',
   components: {
-    HelloWorld,
+    MainLayout,
   },
 };
 </script>
