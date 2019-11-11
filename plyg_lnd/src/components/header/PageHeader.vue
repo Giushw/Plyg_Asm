4f5331553d27e87e3a3aaa5ef230b740fa170da6
giuseppe.dellavvocato@vivaticket.com <dellavvocatog@gmail.com>
2019-11-11 10:43:10 +0100

    chore(): update stle and header

--- plyg_lnd/src/components/header/PageHeader.vue
+++ plyg_lnd/src/components/header/PageHeader.vue
@@ -1,21 +1,23 @@
 <template>
   <header class="page-header">
-    <picture class="header-logo"></picture>
+    <picture class="header-logo">
+      <img src="../../assets/images/logo-playground-white.png" alt="Playground Logo" class="__img">
+    </picture>
     <nav class="header-nav">
-      <div class="navigation-list">
-        <div class="we-are">
-          we-are
-        </div>
-        <div class="we-do">
-          we-do
-        </div>
-        <div class="careers">
+      <ul class="navigation-list">
+        <li class="__item" @click="moveToWeAre">
+          we are
+        </li>
+        <li class="__item">
+          we do
+        </li>
+        <li class="__item">
           careers
-        </div>
-        <div class="contact-us">
-          contact-us
-        </div>
-      </div>
+        </li>
+        <li class="__item">
+          contact us
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
@@ -23,5 +25,10 @@
 <script>
 export default {
   name: 'PageHeader',
+  methods: {
+    moveToWeAre() {
+      // element.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    },
+  },
 };
 </script>
