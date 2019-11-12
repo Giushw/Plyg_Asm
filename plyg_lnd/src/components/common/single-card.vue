fe6d04e0749f315749f94f754aadd0218dd76516
Giushw <dellavvocatog@gmail.com>
2019-11-13 00:56:21 +0100

    feat(): card component

---
+++ plyg_lnd/src/components/common/single-card.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="card-wrapper">
+    <header class="card-heading">
+      <img :src="image" class="__img" :alt="alt">
+    </header>
+    <div class="card-body">
+      <p class="__description">
+        {{ description }}
+      </p>
+    </div>
+    <footer class="card-footer">
+      <button class="__btn -full">
+        {{ cta }}
+      </button>
+    </footer>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'single-card',
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      default: 'Article image',
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    cta: {
+      type: String,
+      default: 'Read More',
+    },
+  },
+};
+</script>
--- plyg_lnd/src/components/content/cards.vue
+++ plyg_lnd/src/components/content/cards.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="cards-wrapper">
-
+    <div class="grid-wrapper ">
+      <div class="grid-row -gutted -padded">
+        <SingleCard v-for="card in infoCards" :key="card.id"
+        class="grid-column"
+        :image="card.image"
+        :description="card.description"
+      />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SingleCard from '../common/single-card.vue';
+
 export default {
   name: 'cards',
+  components: {
+    SingleCard,
+  },
+  data() {
+    return {
+      infoCards: [
+        {
+          image: '../../assets/images/card-play-harder.png',
+          description: 'Aenean mollis dolor a mattis viverra. In hac habitasse platea dictumst. Curabitur tempus dui tortor, et bibendum lacus eleifend ut.',
+        },
+        {
+          image: '../../assets/images/card-card-simplicity.png',
+          description: 'Donec vitae augue tellus. Proin et urna sit amet metus fermentum dapibus non quis urna.',
+        },
+        {
+          image: '../../assets/images/card-innovation.png',
+          description: 'Suspendisse vehicula, lectus sed molestie aliquam, arcu neque gravida lacus.',
+        },
+      ],
+    };
+  },
 };
 </script>
