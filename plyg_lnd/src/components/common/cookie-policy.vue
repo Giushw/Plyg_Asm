c7d047bbcc8816b8c436eb3e66a44ad563ac30ba
giuseppe.dellavvocato@vivaticket.com <dellavvocatog@gmail.com>
2019-11-12 14:39:44 +0100

    feat():grid

--- plyg_lnd/src/components/common/cookie-policy.vue
+++ plyg_lnd/src/components/common/cookie-policy.vue
@@ -3,22 +3,24 @@
     ref="cookiePolicy"
     :class="{ 'is-hidden':cookieSet }"
   >
-    <div class="cookie-wrapper">
-      <p class="cookie-disclaimer -tiny">
-        Our website uses cookies to improve your experience.
-        To find out more about the cookie we use please se our
-        <a href="https://www.playground.it/%20/www.iubenda.com/privacy-policy/43977578/cookie-policy?an=no&s_ck=false"
-          class="__link"
-        >
-          Cookie Policy
-        </a>
-        .
-      </p>
-      <button class="__btn -small"
-        @click="acceptCookies"
-      >
-        Ok
-      </button>
+    <div class="cookie-wrapper grid-wrapper">
+      <div class="grid-row">
+        <div class="grid-column -ninety">
+          <p class="cookie-disclaimer -tiny">
+            Our website uses cookies to improve your experience.
+            To find out more about the cookie we use please se our
+            <a href="https://www.playground.it/%20/www.iubenda.com/privacy-policy/43977578/cookie-policy?an=no&s_ck=false"
+              class="__link"
+            >
+              Cookie Policy
+            </a>
+            .
+          </p>
+        </div>
+        <div class="grid-column">
+          <button class="__btn -small" @click="acceptCookies">Ok</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- plyg_lnd/src/components/header/PageHeader.vue
+++ plyg_lnd/src/components/header/PageHeader.vue
@@ -1,25 +1,32 @@
 <template>
   <header class="page-header">
-    <div class="header-wrapper">
-      <picture class="header-logo">
-      <img src="../../assets/images/logo-playground-white.png" alt="Playground Logo" class="__img">
-    </picture>
-    <nav class="header-nav">
-      <ul class="navigation-list">
-        <li class="__item" @click="moveToWeAre">
-          we are
-        </li>
-        <li class="__item">
-          we do
-        </li>
-        <li class="__item">
-          careers
-        </li>
-        <li class="__item">
-          contact us
-        </li>
-      </ul>
-    </nav>
+    <div class="header-wrapper grid-wrapper">
+      <div class="grid-row -padded -gutted">
+        <div class="grid-column">
+          <picture class="header-logo">
+            <img src="../../assets/images/logo-playground-white.png"
+              alt="Playground Logo" class="__img">
+          </picture>
+        </div>
+        <div class="grid-column">
+          <nav class="header-nav">
+            <ul class="navigation-list">
+              <li class="__item" @click="moveToWeAre">
+                we are
+              </li>
+              <li class="__item">
+                we do
+              </li>
+              <li class="__item">
+                careers
+              </li>
+              <li class="__item">
+                contact us
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   </header>
 </template>
